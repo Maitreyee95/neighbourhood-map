8a73a23415eef02dacb477bb870afe1562ced57f
Maitreyee95 <maitreyee95@gmail.com>
2017-10-14 03:04:01 +0530

    Add filter and show details of locations

---
+++ README.md
@@ -0,0 +1,14 @@
+# Neighbourhood Map Project
+This project shows a map of a neighbourhood in Kolkata along with some locations. You can filter the locations either through the search bar or through the filter menu
+
+## Steps to install and run:
+* Clone or download the repo
+* Unzip if you have downloaded the repo
+* Run index.html
+
+## API used:
+* Google Maps
+* Foursquare venue
+
+## MVVM used:
+* Knockout.js
--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
-
-
+// arraay of locations that would load by efault on page load
 var initialLocations= [
-	{title: "Shahid Minar",location:{lat: 22.5629,lng: 88.3492},type: "monument",placeid: '4c0218258ef2c9b66d9c16fc'},
-	{title: "Indian Museum",location:{lat: 22.5579,lng: 88.3511},type: "poi"},
-	{title: "Eden Gardens",location:{lat: 22.5646,lng: 88.3433},type: "stadium"},
-	{title: "Raj Bhavan",location:{lat: 22.5673,lng: 88.3473},type: "administrative"},
-	{title: "Fort William,India",location:{lat: 22.5542,lng: 88.3359},type: "poi"}
+	{title: "Shaheed Minar",index:1,location:{lat: 22.5629,lng: 88.3492},type: "monument",id:'5292e1a511d20e4e1e12b9c1'},
+	{title: "Indian Museum",index:2,location:{lat: 22.5579,lng: 88.3511},type: "poi",id:'4bf297077f2aef3b05c8392a'},
+	{title: "Eden Garden",index:3,location:{lat: 22.5646,lng: 88.3433},type: "stadium",id:'4bc9c62b511f9521982faec7'},
+	{title: "Raj Bhavan",index:4,location:{lat: 22.5673,lng: 88.3473},type: "administrative",id:'4d283491888af04d4b93c3af'},
+	{title: "Fort William,India",index:5,location:{lat: 22.5542,lng: 88.3359},type: "poi",id:'4d1495d06c8b548133efd7cc'},
+	{title: "Barbeque Nation",index:6,location:{lat: 22.5511,lng: 88.3539},type: "eatery",id:'5598d138498ea6e889b6f0ba'}
 ];
 
 var map;
 var markers=[];
 
+//setting foursquare URL
+var fsUrl = "https://api.foursquare.com/v2/venues/",
+    fsClient_id = "client_id=VZSHPGBA1OXN4OX1BYMZS33X5OH4YANXYLBZL3HNAUNACJKF",
+    fsClient_secret = "&client_secret=RSAKWHS1ZHT0BRQ4P5FVIFQ4CM12PCIBJN3ORLPIB3JKL01H",
+    fsVersion = "&v=20171013";
+
+
+//initialising map
 var initMap=function() {
 	map = new google.maps.Map(document.getElementById('map'),{
 				center: {lat: 22.5570,lng: 88.3510},
@@ -20,6 +28,8 @@ var initMap=function() {
 
 	setEverything();
 };
+
+//places the markers on page
 var setEverything=function(){
 
 	markers.forEach(function(marker){
@@ -27,18 +37,18 @@ var setEverything=function(){
 	});
 	var highlightedIcon="MapMarker_Marker_Outside_Azure.png";
 
-	// var bounds = new google.maps.LatLngBounds();
-
-	markers.forEach(function(marker){
+	var bounds = new google.maps.LatLngBounds();
+	//markers bounce on clicking and shows the infowindow
+	markers.forEach(function(marker,index){
 		// marker.setMap(map);
 		marker.addListener('click', function() {
 			toggleBounce(this);
-			populateInfoWindow(marker, largeInfowindow);
+			populateInfoWindow(marker,index, largeInfowindow);
 		});
-					// bounds.extend(marker.position);
+					bounds.extend(marker.position);
 
 	})
-
+	//adding and removing bounce from markers
 	var toggleBounce=function(marker){
 		if(marker.getAnimation() !== null)
 			{
@@ -52,8 +62,8 @@ var setEverything=function(){
 
 	var largeInfowindow = new google.maps.InfoWindow();
 
-
-	function populateInfoWindow(marker, infowindow) {
+	//making infowindow for each marker
+	function populateInfoWindow(marker,index, infowindow) {
 		// Check to make sure the infowindow is not already opened on this marker.
 		if (infowindow.marker != marker) {
 			infowindow.marker = marker;
@@ -67,12 +77,17 @@ var setEverything=function(){
 			});
 		}
 	}
-	// map.fitBounds(bounds);
+	map.fitBounds(bounds);
 }
+
+//creating a location functinal class using knockout
 var Location=function(data) {
 	this.title=ko.observable(data.title);
 	this.location=ko.observable(data.location);
 	this.type=ko.observable(data.type);
+	this.id=ko.observable(data.id);
+	this.index=ko.observable(data.index);
+	// this.details=ko.observableArray(data.details);
 	this.marker = new google.maps.Marker({
 			position: this.location(),
 			title: this.title(),
@@ -81,36 +96,67 @@ var Location=function(data) {
 	markers.push(this.marker);
 }
 
+
+//knockout viewmodel
 var ViewModel=function () {
-	this.locationTypes=ko.observableArray(['monument','poi','stadium','administrative']);
+	this.locationTypes=ko.observableArray(['monument','poi','stadium','administrative','eatery']);
 	this.chosenType=ko.observableArray([]);
 	this.heading=ko.observable('Welcome to the neighbourhood');
 	this.clickedFilter=ko.observable(false);
 	this.locationList=ko.observableArray([]);
 	var self=this;
 	var locItem=ko.observable();
-	initialLocations.forEach(function(location){
+	initialLocations.forEach(function(location,index){
 		locItem=new Location(location)
 		self.locationList.push(locItem);
-	})
 
+
+	})
+	//when a location is selected or filtered markers are shown on resulted places
 	this.setMarker=function(clickedLocation){
 		hideMarkers();
+		$('#details').empty();
+		var p=clickedLocation.id();
+		var locType=clickedLocation.type();
+		var venueID=p+"/?";
+		var URL= fsUrl + venueID + fsClient_id+ fsClient_secret+fsVersion;
+
+		//setting a timeout function in case foursquare API doesn't load
+		var timeOut=setTimeout(function(){
+			$("#details").text("Failed to get resources");
+		},8000);
+
+		//requesting foursquare  API
+		$.ajax(URL,{
+			dataType: "jsonp",
+			success: function(data){
+
+				a=data.response.venue.likes.summary;
+				b = data.response.venue.rating;
+				c= data.response.venue.name;
+				d=data.response.venue.location.formattedAddress;
+				//showing details of clicked location
+				$('#details').append('<div>'+c+'</div>Address:'+d+'<div>Rating:'+b+'</div><div>Likes:'+a+'</div><div>Type:'+locType+'</div>');
+				//clearing the timeout if request comes back successful
+				clearTimeout(timeOut);
+			}
+		})
 		clickedLocation.marker.setMap(map);
 	};
-
+	//function for hiding the markers
 	var hideMarkers=function(){
 		markers.forEach(function(marker){
 			marker.setMap(null);
 		})
 	};
+
+	//filter data on selecting types from filter menu
 	this.filterData=function(){
 		hideMarkers();
 		markers=[];
-		// var filteredType=self.chosenType()[0];
 		showFilteredList(self.chosenType()[0]);
 	}
-
+	//showing and hiding menu on cklicking 'Filter'
 	self.filterclicked=function(){
 		if (self.clickedFilter()===true){
 			self.clickedFilter(false);
@@ -119,17 +165,30 @@ var ViewModel=function () {
 		}
 
 	}
-
+	//filter data on writing in search box
 	this.filterSearchData=function(){
-		hideMarkers();
 
-		markers=[];
+		var call=false;
+
 		var filteredType= document.getElementById('choice').value;
-		showFilteredList(filteredType);
+		this.locationTypes().forEach(function(type){
+			if(filteredType===type){
+				call=true;
+				hideMarkers();
+				markers=[];
+				showFilteredList(filteredType);
+			}
+		})
+		if (call===false){
+			alert("Sorry!No result found. Check search keyword and try again!");
+		}
 	}
+
+	//showing the results of filtering
 
 	var showFilteredList=function(filteredType){
 		var c=1;
+		$("#details").empty();
 		var loc=ko.observable();
 		self.locationList.removeAll();
 		markers=[];
@@ -145,6 +204,8 @@ var ViewModel=function () {
 		}
 		setEverything();
 	};
+
+	//resetting map and details on clicking "See All"
 	this.reset=function(){
 		self.locationList.removeAll();
 		hideMarkers();
@@ -152,20 +213,24 @@ var ViewModel=function () {
 		initialLocations.forEach(function(location){
 			self.locationList.push(new Location(location));
 		})
+		$('#details').empty();
 		setEverything();
 	};
-
 };
 
+// function for opening the navigation bar on small screen
 function openNav() {
     document.getElementById("mySidenav").style.width = "50vw";
 }
 
-/* Set the width of the side navigation to 0 */
+//closing navigation bar on hitting "close" in small screen
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
+
+
+//start() runs when map loads
 var start=function(){
 	ko.applyBindings(new ViewModel());
 	initMap();
